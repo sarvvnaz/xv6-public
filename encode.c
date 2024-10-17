4ca56ea90a76d92e6b2ea3063232befa73b4435f
Arman <121860974+RizzZero@users.noreply.github.com>
2024-10-17 22:28:19 +0330

    Update encode.c

--- encode.c
+++ encode.c
@@ -1,25 +1,30 @@
 #include "types.h"
 #include "stat.h"
-#include "fcntl.h"
 #include "user.h"
+#include "fcntl.h"
 
-void encode(char text[]) {
-    for (int i = 0; text[i] != '\0'; ++i) {
-        char c = text[i];
-        if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z')) {
-            char base = (c >= 'A' && c <= 'Z') ? 'A' : 'a';
-            text[i] = (c - base + 11) % 26 + base;
-        }
-    }
-}
-
-int main() {
-    char text[100] ;
-    gets(text,sizeof(text));
-    encode(text);
-    int f ;
-    f = open("result.txt",O_CREATE|O_RDWR);
-    write(f,text,strlen(text));
-    close(f);
-    return 0;
+int
+main(int argc, char *argv[])
+{
+  int i;
+  for(i = 1; i < argc ; i++)
+  {
+  int s = strlen(argv[i]);
+  int j;
+  	for (j = 0; j < s ; j ++){
+	    if ((argv[i][j] >= 'A' && argv[i][j] <= 'Z') || (argv[i][j] >= 'a' && argv[i][j] <= 'z'))
+	    	{
+	    		char base = (argv[i][j] >= 'A' && argv[i][j] <= 'Z') ? 'A' : 'a';
+	    		argv[i][j] = (argv[i][j] - base + 11) %26 + base;
+	    	}
+	    }
+  }
+  int f ;
+  f = open("result.txt",O_CREATE|O_RDWR);
+  int k;
+  for(k = 1; k < argc; k++){
+	  write(f,argv[k],strlen(argv[k]));
+ 	 write(f," ",1);}
+  close(f);
+  exit();
 }
