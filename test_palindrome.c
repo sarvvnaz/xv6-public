bf7a1b2fc682ac5327ccd3162fb282324b53ac96
Arman <121860974+RizzZero@users.noreply.github.com>
2024-11-09 15:15:54 +0330

    Update test_palindrome.c
    test palindrome completed!

--- test_palindrome.c
+++ test_palindrome.c
@@ -6,17 +6,30 @@ int
 main(int argc, char* argv[]){
 	if (argc!=2){
 		if (argc < 2){
-			printf(1,"number where?");
+			printf(1,"number where?\n");
 			}
 		else if (argc>2){
-			printf(1,"enough argumants");
+			printf(1,"too many argumants\n");
 			}
 		exit();
 		}
 	else {
 		int preEBX ;
 		int num = atoi(argv[1]);
-			printf(1,"USER: create_palindrome() is called for n = %d\n",num);
+		asm volatile(
+			"movl %%ebx, %0;"
+			"movl %1, %%ebx;"
+			: "=r" (preEBX)
+			: "r"(num)
+			);
+		printf(1,"USER: create_palindrome() is called for n = %d\n",num);
+		int answer = create_palindrome();
+		printf(1,"palindrome of %d is %d\n",num,answer);
+		asm(
+			"movl %0, %%ebx"
+			:
+			: "r"(preEBX)
+		);
 		}
 		exit();
 		}
