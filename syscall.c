7bc9f9520e6a03685b7a0bb8579075e7042045fd
Arman <121860974+RizzZero@users.noreply.github.com>
2024-11-09 19:23:48 +0330

    Update syscall.c
    sys_save_pid_and_calls protottype added!

--- syscall.c
+++ syscall.c
@@ -104,7 +104,7 @@ extern int sys_wait(void);
 extern int sys_write(void);
 extern int sys_uptime(void);
 extern int sys_create_palindrome(void);
-
+extern int sys_save_pid_and_calls(void);
 
 static int (*syscalls[])(void) = {
 [SYS_fork]    sys_fork,
