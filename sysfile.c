505d48fb44bbc3e36a4793ef76a209a7c8541db7
Sarvenaz Salehi <113339568+sarvvnaz@users.noreply.github.com>
2024-11-11 00:22:12 +0330

    Update sysfile.c

--- sysfile.c
+++ sysfile.c
@@ -16,6 +16,7 @@
 #include "file.h"
 #include "fcntl.h"
 
+int move_file(char *src_file, char *dest_dir);
 // Fetch the nth word-sized system call argument as a file descriptor
 // and return both the descriptor and the corresponding struct file.
 static int
