395b6c8f9c42a17362be059fef5488171a7bec03
Sarvenaz Salehi <113339568+sarvvnaz@users.noreply.github.com>
2024-11-11 00:19:36 +0330

    Update user.h

--- user.h
+++ user.h
@@ -1,5 +1,6 @@
 struct stat;
 struct rtcdate;
+struct proc;
 
 // system calls
 int fork(void);
@@ -23,6 +24,13 @@ int getpid(void);
 char* sbrk(int);
 int sleep(int);
 int uptime(void);
+int sort_syscalls(int pid);
+int get_process_by_pid(int, struct proc**);
+int get_most_invoked_syscall(int);
+int list_all_processes(void);
+int move_file(const char *src_file, const char *dest_dir);
+void find_palindrome(int);
+
 
 // ulib.c
 int stat(const char*, struct stat*);
